062b7ed8ecd9f560355c7608ceddd5c52b5dc574
zztkm <takumi.basket1682@gmail.com>
2021-11-09 13:32:50 +0900

    method 変更
    get -> post

--- src/app.py
+++ src/app.py
@@ -8,16 +8,16 @@ from .ssl_check import bulk_check_ssl, check_ssl
 app = FastAPI(
     title="SSL Check API",
     description="collects SSL/TLS information from hosts",
-    version="0.4.0",
+    version="0.5.0",
 )
 
 
-@app.get("/isssl/", response_model=ValidResult, response_description="SSL対応しているか判定した結果")
+@app.post("/isssl/", response_model=ValidResult, response_description="SSL対応しているか判定した結果")
 async def is_ssl(host: str):
     return check_ssl(host)
 
 
-@app.get(
+@app.post(
     "/isssl/bulk/",
     response_model=List[ValidResult],
     response_description="SSL対応しているか判定した結果",
